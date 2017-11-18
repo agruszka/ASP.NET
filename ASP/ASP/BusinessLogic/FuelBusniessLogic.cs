68d96153b534e54b3c7b0bec3a2cdcdc6696dba9
Jacek Gmoch <jacek.gmoch@gmail.com>
2017-11-18 11:34:50 +0100

    FuelCalculator

---
+++ ASP/ASP/BusinessLogic/FuelBusniessLogic.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASP.BusinessLogic
+{
+    public class FuelBusniessLogic
+    {
+        public double CalculateAvgConsumption(int fuel, int distance)
+        {
+            double avg = (double)fuel / distance;
+            return avg * 100;
+        }
+    }
+}
---
+++ ASP/ASP/Controllers/FuelController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ASP.BusinessLogic;
+using ASP.Models;
+using ASP.ViewModel;
+
+namespace ASP.Controllers
+{
+    public class FuelController : Controller
+    {
+        private readonly FuelBusniessLogic _businessLogic;
+        public FuelController()
+        {
+            _businessLogic = new FuelBusniessLogic();
+        }
+        // GET: Fuel
+        public ActionResult Index()
+        {
+            var model = new FuelViewModel {FuelModel = new FuelModel() };
+            return View("View",model);
+        }
+        [HttpPost]
+        public ActionResult FuelCalculate(FuelViewModel model)
+
+        {
+            if (model.FuelModel!=null&& model.FuelModel.Distance!=0)
+            {
+                model.FuelModel.AvgConsumption = _businessLogic.CalculateAvgConsumption(model.FuelModel.Fuel, model.FuelModel.Distance);
+                model.ShowResult = true;
+                return View("View", model);
+            }
+            else
+            {
+                model.ShowResult = false;
+                ViewBag.ERROR = "Błędne dane";
+                return View("View");
+
+            }
+        }
+    }
+}
