e62501f04fc0ea0e870650e415a26f5dc3297a1c
Jacek Gmoch <jacek.gmoch@gmail.com>
2017-11-18 09:48:15 +0100

    Dodana klasa w folderze ViewModel

---
+++ ASP/ASP/ViewModel/FuelViewModel.cs
@@ -0,0 +1,14 @@
+﻿using ASP.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASP.ViewModel
+{
+    public class FuelViewModel
+    {
+        public FuelModel FuelModel { get; set; }
+        public bool ShowResult { get; set; }
+    }
+}
